e5b0e0cd9408a5414755ad9066d330095b27d139
Moeein Aali <moeeeinaali@gmail.com>
2025-08-29 14:54:24 +0330

    [ADD] tailwind v4

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
     return (
-        <h1 className="text-3xl font-bold underline text-primary-content bg-primary">
-            Hello world!
-        </h1>
+        <>
+            <button className={"bg-primary w-80 h-20 text-primary-content"}>click</button>
+            <button className={"bg-primary-focus w-80 h-20"}>click</button>
+            <button className={"bg-primary-content w-80 h-20"}>click</button>
+        </>
     );
 }
