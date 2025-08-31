e7b3d7ca8102212b864e9fe94017c46097518e32
Moeein Aali <moeeeinaali@gmail.com>
2025-08-31 10:18:11 +0330

    [FEAT] add another tests for Button

--- src/ui/components/button/button.test.tsx
+++ src/ui/components/button/button.test.tsx
@@ -1,5 +1,22 @@
-describe('Button', () => {
-    it('testing button', () => {
+import {render, screen} from "@testing-library/react";
+import Button from "./button.component";
 
+describe('Button Component', () => {
+    test('renders a default button', () => {
+        const {getByText} = render(<Button>Click</Button>);
+        expect(getByText("Click")).toBeInTheDocument()
+    })
+
+    test('disables the button when isDisabled prop is true', () => {
+        render(<Button isDisabled={true}>Click</Button>);
+        expect(screen.getByRole('button')).toBeDisabled()
+    })
+
+    test("applies the correct css class for different buttons varient", () => {
+        const {rerender} = render(<Button variant={"primary"}>click!</Button>)
+        expect(screen.getByRole('button')).toHaveClass("btn-primary")
+
+        rerender(<Button variant={"info"}>click!</Button>)
+        expect(screen.getByRole('button')).toHaveClass("btn-info")
     })
 })
