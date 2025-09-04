09d9b5c1fa8528c37ee18d631650d4d0c6e56551
Moeein Aali <moeeeinaali@gmail.com>
2025-09-04 15:08:23 +0330

    [FEAT] add h2 section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,8 @@ import { HomeHeroSection } from "@/app/_components/home-hero-section/home-hero-s
 import CourseCardList from "@/app/_components/course-card-list/course-card-list";
 import { homeFeatures } from "@/lib/data/home-features";
 import Feature from "./_components/feature/feature";
+import Button from "@/ui/components/button/button.component";
+import { IconArrowLeftFill } from "@/ui/components/icons/icons";
 
 
 async function getNewestCourses(count: number): Promise<CourseSummary[]> {
@@ -25,7 +27,7 @@ export default async function HomePage() {
                     }
                 </div>
             </section>
-            
+
             <section className="container pt-20">
                 <div className="text-center xl:text-right">
                     <h2 className="text-2xl font-extrabold">
@@ -36,6 +38,44 @@ export default async function HomePage() {
                     </p>
                 </div>
                 <CourseCardList courses={newestCourses} />
+            </section>
+            <section className="px-2 my-40">
+                <div className="relative pt-0 text-center">
+                    <div className="bg-primary pointer-events-none absolute left-1/2 aspect-square w-1/2 -translate-x-1/2 -top-96 rounded-full opacity-10 blur-3xl"></div>
+
+                    <h2
+                        lang="en"
+                        className="gradient leading-[1.3] relative z-10 mx-auto inline-block text-[clamp(2rem,6vw,5.5rem)] font-black"
+                    >
+                        ReactJs & Next.js
+                    </h2>
+                    <p className="text-base-content/70  relative z-[2] py-4 m-auto md:text-3xl max-w-5xl font-light !leading-[1.7]">
+                        ری‌اکت و نکست‌جی‌اس برترین کتابخونه‌های فرانت‌اند و
+                        یکه‌تاز دنیای وب! پیشرفته‌ترین مباحث رو اینجا می تونی
+                        پیدا کنی. پس همین الان یادگیری رو شروع کن ما هم از
+                        ابتدای مسیر با آموزش‌های تخصصی و کاملاً کاربردی کنارت
+                        هستیم.
+                    </p>
+                    <div className="flex flex-col lg:flex-row items-center gap-3 justify-center">
+                        <Button
+                            variant="primary"
+                            size="large"
+                            className="mt-7"
+                            isIconAnimated={true}
+                        >
+                            دوره‌های ری اکت و نکست‌ جی‌اس
+                            <IconArrowLeftFill fill="currentColor" />
+                        </Button>
+                        <Button
+                            variant="neutral"
+                            size="large"
+                            className="mt-7"
+                            isIconAnimated={true}
+                        >
+                            مقالات ری اکت و نکست‌ جی‌اس
+                        </Button>
+                    </div>
+                </div>
             </section>
         </>
     );
