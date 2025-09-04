5ade4287fed058e9f1d4c075913327d9751e819e
Moeein Aali <moeeeinaali@gmail.com>
2025-09-04 19:51:42 +0330

    [REFACTOR] use avatar for testimontial

--- src/app/_components/testimonial/testimonial-item.tsx
+++ src/app/_components/testimonial/testimonial-item.tsx
@@ -1,5 +1,5 @@
-import Image from "next/image";
 import { TestimonialProps } from "./testimonial.types";
+import Avatar from "@/ui/components/avatar/avatar.component";
 
 const TestimonialItem: React.FC<TestimonialProps> = ({
     testimonial: { image, name, skills, description },
@@ -7,14 +7,7 @@ const TestimonialItem: React.FC<TestimonialProps> = ({
     return (
         <div className="border-base-content/5 border p-4 rounded-2xl text-sm hover:border-base-content/30 hover:bg-base-content/5 blur-[1px] hover:blur-none">
             <div className="flex items-center gap-2">
-                <Image
-                    loading="lazy"
-                    src={image}
-                    alt={name}
-                    width="48"
-                    height="48"
-                    className="pointer-events-none rounded-full grayscale"
-                />
+                <Avatar src={image} alt="name" size="tiny" className="pointer-events-none rounded-full grayscale" />
                 <div className="flex flex-col items-start font-semibold">
                     <div className="text-info/80">{name}</div>
                     <div className="text-base-content/60">{skills}</div>
