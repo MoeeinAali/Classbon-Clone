ce13421a4a7b0e6d8565a02ccbfdd89aaa6b51e8
Moeein Aali <moeeeinaali@gmail.com>
2025-09-05 16:09:26 +0330

    [FEAT] add generateStaticParams to course details page

--- src/app/(courses)/courses/[slug]/page.tsx
+++ src/app/(courses)/courses/[slug]/page.tsx
@@ -1,3 +1,25 @@
-export default async function CourseDetailPage() {
-    return <div></div>;
+import { API_URL } from "@/lib/configs/global";
+import { CourseDetails } from "@/lib/types/course-details.dto";
+
+
+export async function generateStaticParams() {
+    const response = await fetch(`${API_URL}/courses/slugs/`);
+    const slugs = await response.json();
+
+    return (slugs as string[]).map((slug) => ({
+        slug: slug
+    }));
+
+}
+
+async function getCourse(slug: string): Promise<CourseDetails> {
+    const response = await fetch(`${API_URL}/courses/${slug}`);
+    const data = await response.json();
+    return data;
+}
+
+export default async function CourseDetailPage({ params }: { params: { slug: string } }) {
+    const { slug } = await params;
+    const course = await getCourse(slug);
+    return <div>{course.title}</div>;
 }
