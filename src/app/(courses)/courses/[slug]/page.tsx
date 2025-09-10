c45f2370b4162fd6ed0e08d3d4e733e76623317b
Moeein Aali <moeeeinaali@gmail.com>
2025-09-10 14:02:55 +0330

    [FIX] params type

--- src/app/(courses)/courses/[slug]/page.tsx
+++ src/app/(courses)/courses/[slug]/page.tsx
@@ -1,19 +1,21 @@
-import { API_URL } from "@/lib/configs/global";
-import { CourseDetails } from "@/lib/types/course-details.dto";
-import Progress from "@/ui/components/progress/progress.component";
-import Rating from "@/ui/components/rating/rating.component";
+import {API_URL} from "@/lib/configs/global";
+import {CourseDetails} from "@/lib/types/course-details.dto";
 import CourseAside from "../../_components/course-aside";
-import { Tab } from "@/lib/types/tab.type";
+import {Tab} from "@/lib/types/tab.type";
 import Tabs from "@/ui/components/tab/tabs.component";
 
 
 export async function generateStaticParams() {
-    const response = await fetch(`${API_URL}/courses/slugs/`);
-    const slugs = await response.json();
+    try {
+        const response = await fetch(`${API_URL}/courses/slugs/`);
+        const slugs = await response.json();
 
-    return (slugs as string[]).map((slug) => ({
-        slug: slug
-    }));
+        return (slugs as string[]).map((slug) => ({
+            slug: slug
+        }));
+    } catch {
+        return [];
+    }
 
 }
 
@@ -23,8 +25,11 @@ async function getCourse(slug: string): Promise<CourseDetails> {
     return data;
 }
 
-export default async function CourseDetailPage({ params }: { params: { slug: string } }) {
-    const { slug } = await params;
+export default async function CourseDetailPage(
+    {params}: {
+        params: Promise<{ slug: string }>
+    }) {
+    const {slug} = await params;
     const course = await getCourse(slug);
 
     const tabs: Tab[] = [
@@ -44,7 +49,8 @@ export default async function CourseDetailPage({ params }: { params: { slug: str
 
     return (
         <div className="container grid grid-cols-10 grid-rows-[1fr 1fr] gap-10 py-10">
-            <div className="bg-primary pointer-events-none absolute right-0 aspect-square w-1/2 rounded-full opacity-10 blur-3xl"></div>
+            <div
+                className="bg-primary pointer-events-none absolute right-0 aspect-square w-1/2 rounded-full opacity-10 blur-3xl"></div>
 
             <div className="col-span-10 xl:col-span-7">
                 <h1 className="text-center xl:text-right text-2xl lg:text-3xl xl:text-4xl font-black leading-10">
@@ -56,15 +62,15 @@ export default async function CourseDetailPage({ params }: { params: { slug: str
 
                 <div className="mt-5">Video Player Component</div>
             </div>
-            
+
             <div className="col-span-10 xl:col-span-3">
                 <CourseAside {...course} />
             </div>
-            
+
             <div className="col-span-10 xl:col-span-6">
-                <Tabs tabs={tabs} />
+                <Tabs tabs={tabs}/>
             </div>
-            
+
             <div className="col-span-10 xl:col-span-4">salam</div>
         </div>
     )
