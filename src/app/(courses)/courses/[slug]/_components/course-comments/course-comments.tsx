6a97d67d027b376115459a2c475db8e7b89979ab
Moeein Aali <moeeeinaali@gmail.com>
2025-09-11 18:21:38 +0330

    [FEAT] add pages to course-comment component

--- src/app/(courses)/courses/[slug]/_components/course-comments/course-comments.tsx
+++ src/app/(courses)/courses/[slug]/_components/course-comments/course-comments.tsx
@@ -4,32 +4,44 @@ import {useParams} from "next/navigation";
 import {useCourseComments} from "@/app/(courses)/courses/[slug]/_api/get-comments";
 import {Comment} from "@/ui/components/comment/comment.component";
 import {TextPlaceholder} from "@/ui/components/placeholders";
-import {Fragment} from "react";
+import {Fragment, useEffect} from "react";
+import {useInView} from "react-intersection-observer";
 
 export const CourseComments = () => {
+    const {ref, inView} = useInView({})
     const {slug} = useParams();
-    const {data: comments, isLoading} = useCourseComments({
+
+    const {data: comments, isLoading, isFetchingNextPage, hasNextPage, fetchNextPage} = useCourseComments({
         params: {
             slug: slug as string,
             page: 1
         }
     });
+
+    useEffect(() => {
+        if (inView && hasNextPage) {
+            fetchNextPage().then();
+        }
+    }, [inView, fetchNextPage, hasNextPage]);
+
     return (
         <>
             {
-                isLoading && <TextPlaceholder count={10}/>
-            }
-
-            {
-                comments?.pages.map((currentPage) => (
-                    <Fragment key={`comment-page-${currentPage}`}>
+                comments?.pages.map((page, index) => (
+                    <Fragment key={`comment-page-${comments.pageParams?.[index] ?? index}`}>
                         {
-                            currentPage.data.map((item) => (
+                            page.data.map((item) => (
                                 <Comment key={`comment-${item.id}`} {...item} variant={"info"}/>
                             ))
                         }
                     </Fragment>
                 ))
+            }
+            {
+                (isFetchingNextPage || hasNextPage) &&
+                <div ref={ref}>
+                    <TextPlaceholder count={1}/>
+                </div>
             }
         </>
     )
