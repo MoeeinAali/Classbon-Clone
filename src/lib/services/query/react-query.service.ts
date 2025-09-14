1d85caa9b9971b3ffca290d0de1519c2571aa0f9
Moeein Aali <moeeeinaali@gmail.com>
2025-09-15 00:15:09 +0330

    [FEAT] add handleProblemNotification

--- src/lib/services/query/react-query.service.ts
+++ src/lib/services/query/react-query.service.ts
@@ -1,11 +1,14 @@
 "use client"
 
 import {QueryCache, QueryClient} from "@tanstack/react-query";
+import {showNotifications} from "@/lib/stores/notification.store";
+import {Problem} from "@/lib/types/http-errors.interface";
+import {Notification} from "@/lib/types/notification.interface";
 
 export const queryClient = new QueryClient({
     queryCache: new QueryCache({
-        onError: (err) => {
-            //     TODO: Notification pop up...
+        onError: (error) => {
+            handleProblemNotification(error as unknown as Problem);
         }
     }),
 
@@ -19,3 +22,26 @@ export const queryClient = new QueryClient({
         }
     }
 })
+
+
+const handleProblemNotification = (problem: Problem) => {
+    const notifications: Omit<Notification, 'id'>[] = []
+
+    if (problem?.errors) {
+        Object.entries(problem.errors).forEach(([_, error]) => {
+            error.forEach((errorMessage) => {
+                notifications.push({
+                    type: "error",
+                    message: errorMessage,
+                })
+            })
+        })
+    } else if (problem?.detail) {
+        notifications.push({
+            type: "error",
+            message: problem.detail,
+        })
+    }
+
+    showNotifications(notifications);
+}
